c02407040633638f436da1631d9edb05702718ce
Sakshi80506 <sakshikamble0506@gmail.com>
2026-09-15 13:30:12 +0530

    final changes

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -33,12 +33,12 @@ import {
 } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyCR99KFaf6cLehmM_1PIPTBYebwT1nTpM8",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "kaarigar-expo.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "kaarigar-expo",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "kaarigar-expo.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "371615538367",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:371615538367:web:a2e22e531ab26c3b5bfa0a"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
